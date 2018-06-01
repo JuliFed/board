8d061612ac5bb6b4ef37375734fef6c8b85ae0fa
JuliFed <fedorchenkojuli@gmail.com>
2018-06-02 02:53:25 +0300

    Added comments and views

--- board.py
+++ board.py
@@ -9,18 +9,20 @@ from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 
 
-def is_valid_data(creator, board_name):
+def is_valid_board(creator, board_name):
+    if not creator or not board_name:
+        return False
+    if len(creator) > 30 or len(board_name) > 50:
+        return False
     return True
 
-def base36_encode(number):
-    assert number >= 0, 'positive integer required'
-    if number == 0:
-        return '0'
-    base36 = []
-    while number != 0:
-        number, i = divmod(number, 36)
-        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
-        return ''.join(reversed(base36))
+
+def is_valid_comment(creator, comment):
+    if not creator or not comment:
+        return False
+    if len(creator) > 30 or len(comment) > 255:
+        return False
+    return True
 
 
 class Board(object):
@@ -32,7 +34,6 @@ class Board(object):
         self.url_map = Map([
             Rule('/', endpoint='new_boards'),
             Rule('/<board_id>', endpoint='board_details'),
-            Rule('/<board_id>+', endpoint='add_comment_to_board')
         ])
 
     def render_template(self, template_name, **context):
@@ -60,41 +61,76 @@ class Board(object):
         if request.method == 'POST':
             creator = request.form['creator']
             board_name = request.form['board_name']
-            if not is_valid_data(creator, board_name):
+            if not is_valid_board(creator, board_name):
                 error = 'Please enter a valid data'
             else:
                 board_id = self.insert_new_desk(creator, board_name)
-                return redirect('/%s' % board_id, error=error)
+                return redirect('/%s' % board_id)
                 
         boards = self.redis.keys('board:*')
+        boards.sort()
         boards_list = []
         for board in boards:
             board_name = self.redis.get(board)
             creator = self.redis.get('creator:' + board)
+            creation_date =  self.redis.get('creation_date:' + board)
             board_id = board.split(':')[1]
-            boards_list.append({'id': board_id, 'name': board_name, "creator": creator})
+            boards_list.append({'id': board_id, 'name': board_name, "creator": creator, "dt": creation_date})
         return self.render_template('boards.html', boards_list=boards_list, error=error)
     
     def on_board_details(self, request, board_id):
-        print(board_id)
+        error = None
+        if request.method == 'POST':
+            creator = request.form['creator']
+            new_comment = request.form['comment']
+            if not is_valid_comment(creator, new_comment):
+                error = 'Please enter a valid data'
+            else:
+                self.insert_new_comment(board_id, creator, new_comment)
+                return redirect('/%s' % board_id)
+            
         board_name = self.redis.get('board:' + board_id)
         creator = self.redis.get('creator:board:' + board_id)
+        creation_date = self.redis.get('creation_date:board:' + board_id)
         if board_name is None:
             raise NotFound()
-        print(board_name)
-        comments =
-        return self.render_template('boards.html',
+        
+        comments = self.redis.keys("comment:*:board:" + board_id)
+        count_comm = len(comments)
+        comments_list = []
+        for comment in comments:
+            comment_data = self.redis.get(comment)
+            comment_id = comment.split(':')[1]
+            comm_creator = self.redis.get('creator:comment:' + comment_id)
+            comm_dt = self.redis.get('creation_date:comment:' + comment_id)
+            comments_list.append({'id': comment_id,
+                                  'comment_data': comment_data,
+                                  "comm_creator": comm_creator,
+                                  'dt': comm_dt
+                                  })
+
+        return self.render_template('board_detail.html',
                                     board_name=board_name,
+                                    dt=creation_date,
                                     creator=creator,
-                                    comments=comments)
+                                    count_comm=count_comm,
+                                    comments=comments_list)
 
     def insert_new_desk(self, creator, board_name):
         board_id = str(self.redis.incr('last-desk-id'))
         
         self.redis.set('board:' + board_id, board_name)
         self.redis.set('creator:board:' + board_id, creator)
-        self.redis.set('creation_date:board:' + board_id, datetime.now())
-        
+        self.redis.set('creation_date:board:' + board_id, datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S"))
+        return board_id
+
+    def insert_new_comment(self, board_id, creator, comment):
+        comment_id = str(self.redis.incr('last-comment-id'))
+
+        self.redis.set('comment:' + comment_id + ':board:' + board_id, comment)
+        self.redis.set('creator:comment:' + comment_id, creator)
+        self.redis.set('creation_date:comment:' + comment_id, datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"))
+
         return board_id
 
 
